52bb9cb0a9ba5ecf93411f9e4c86a7fb373249a1
Margot Duek <duekis_94@hotmail.com>
2017-03-29 15:19:34 -0600

    change order of the array

--- segundo-parcial/exam/slot.c
+++ segundo-parcial/exam/slot.c
@@ -10,16 +10,20 @@
 #include <pthread.h>
 #include <time.h>
 
+#define ARRAY_SIZE 6
+
 // Function declarations
 void * start_slot(void * arg);
+void *mix_array(int num, char *chars);
 
-char temp[6];
+char temp[ARRAY_SIZE];
+pthread_mutex_t mutex_1 = PTHREAD_MUTEX_INITIALIZER;
 
 int main(){
   int status;
-  int f_th = 1;
-  int s_th = 2;
-  int th_th = 3;
+  int f_th = 4;
+  int s_th = 3;
+  int th_th = 5;
   
   pthread_t first_slot, second_slot, third_slot;
   status = pthread_create(&first_slot, NULL, &start_slot, &f_th);
@@ -29,6 +33,7 @@ int main(){
       exit(EXIT_FAILURE);
     }
   printf("Created new thread \n");
+  
   status = pthread_create(&second_slot, NULL, &start_slot, &s_th);
   if (status)
     {
@@ -43,7 +48,7 @@ int main(){
       exit(EXIT_FAILURE);
     }
   printf("Created new thread \n");
-
+  
 
   // Wait for all other threads to finish
   pthread_exit(NULL);
@@ -56,21 +61,45 @@ int main(){
 
 void * start_slot(void * arg){
   int num = *((int *)arg);
-  printf("num  %d\n", num);
+  //printf("num  %d\n", num);
   
   srand(time(NULL));
   char chars[] = {'M', 'A', 'R', 'G', 'O', 'T'};
-  int stp = rand() * num;
-  printf("nu  %d\n", stp);
-  
-  int choosed = 0;
-  for( int i = 0; i < stp; i ++){
-    if(choosed > 5){
-      choosed = 0;
-    }
-    choosed ++;
+  while(running){
+    // Lock before the critical section
+    pthread_mutex_lock(&mutex_1);
+    mix_array(num, chars);
+    // Unlock after the task is completed
+    pthread_mutex_unlock(&mutex_1);
   }
-
-  printf("num is %d\n", choosed);
+  
+  for(int i = 0; i < ARRAY_SIZE; i++){
+    printf("%c", chars[i]);
+  }
+  
   return NULL;
+}
+
+void *mix_array(int num, char *chars){
+  int x;
+  int restarted = 0;
+  for(int i = 0; i < ARRAY_SIZE ; i++){
+
+    if( x < ARRAY_SIZE && restarted){
+      x++;
+    }
+    else if (x >= ARRAY_SIZE - 1){
+      x = 0;
+      restarted = 1;
+    }else if(x < ARRAY_SIZE && !restarted){
+      x = i + num;
+    }
+    temp[i] = chars[x];
+    //    printf("chars %c\n", chars[i]);
+    //printf("temp %c\n", temp[i]);
+  }
+  for(int i = 0; i < ARRAY_SIZE; i++){
+    chars[i] = temp[i];
+  }
+  
 }
