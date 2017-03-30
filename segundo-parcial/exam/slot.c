d765b895e2a3966fa11c934679c88bf78028bee9
Margot Duek <duekis_94@hotmail.com>
2017-03-29 18:16:32 -0600

    :sad: user thread and user_interface functions added (not working)

--- segundo-parcial/exam/slot.c
+++ segundo-parcial/exam/slot.c
@@ -15,17 +15,21 @@
 // Function declarations
 void * start_slot(void * arg);
 void *mix_array(int num, char *chars);
+void *user_interface(void *a);
 
 char temp[ARRAY_SIZE];
 pthread_mutex_t mutex_1 = PTHREAD_MUTEX_INITIALIZER;
 
+
+
 int main(){
   int status;
-  int f_th = 4;
+  int f_th = 1;
   int s_th = 3;
   int th_th = 5;
   
-  pthread_t first_slot, second_slot, third_slot;
+  pthread_t first_slot, second_slot, third_slot, user_thread;
+
   status = pthread_create(&first_slot, NULL, &start_slot, &f_th);
   if (status)
     {
@@ -48,6 +52,13 @@ int main(){
       exit(EXIT_FAILURE);
     }
   printf("Created new thread \n");
+  status = pthread_create(&user_thread, NULL, &user_interface, &th_th);
+  if (status)
+    {
+      fprintf(stderr, "ERROR: pthread_create %d\n", status);
+      exit(EXIT_FAILURE);
+    }
+  printf("Created new thread \n");  
   
 
   // Wait for all other threads to finish
@@ -58,24 +69,27 @@ int main(){
 }
 
 
-
 void * start_slot(void * arg){
   int num = *((int *)arg);
   //printf("num  %d\n", num);
   
-  srand(time(NULL));
+  
   char chars[] = {'M', 'A', 'R', 'G', 'O', 'T'};
-  while(running){
+  
+  if(num < 5 && num > 0){
     // Lock before the critical section
     pthread_mutex_lock(&mutex_1);
     mix_array(num, chars);
     // Unlock after the task is completed
     pthread_mutex_unlock(&mutex_1);
+  }else if (num >= 20){
+    for(int i = 0; i < ARRAY_SIZE; i++){
+      printf("%c", chars[i]);
+    }
+    num = 1;
+    user_interface(&num);
   }
   
-  for(int i = 0; i < ARRAY_SIZE; i++){
-    printf("%c", chars[i]);
-  }
   
   return NULL;
 }
@@ -95,11 +109,16 @@ void *mix_array(int num, char *chars){
       x = i + num;
     }
     temp[i] = chars[x];
-    //    printf("chars %c\n", chars[i]);
-    //printf("temp %c\n", temp[i]);
   }
   for(int i = 0; i < ARRAY_SIZE; i++){
     chars[i] = temp[i];
   }
   
+}
+
+void *user_interface(void *a){
+  int option;
+  printf("press \n -1 to stop the game \n 21 (our lucky unmber) to watch your slots ");
+  scanf("%d", &option);
+  start_slot(&option);
 }
